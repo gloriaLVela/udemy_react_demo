f2cbc866cde789b157a8b527b37443580a0add90
Laura Work <Work@Glorias-Mac-mini.local>
2020-08-05 08:01:52 -0500

    completed React Life cycle

--- src/Application.js
+++ src/Application.js
@@ -2,8 +2,36 @@ import React, {Component} from 'react';
 
 class Application extends Component {
 
+    constructor(props){
+        // Runs every time the component is instanciated
+        super(props);
+
+
+    }
+
+    UNSAFE_componentWillMount(props, state){
+
+    }
+
+    componentDidMount(props, state){
+
+        console.log("Mounted With", props, state);
+    }
+
+    UNSAFE_componentWillReceiveProps(props){
+
+    }
+
     render(){
-        return 'Hello World!';
+        let name="Bob";
+
+        return(
+            <div>
+            <h1>Hello, {name}!!!!</h1>
+            <span>this</span>
+            </div>
+        )
+       
     }
 
 }
