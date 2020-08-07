4fec6bebcdfa90b2e84a9727b02bf6fd6303cd14
Laura Work <Work@Glorias-Mac-mini.local>
2020-08-07 14:36:26 -0500

    highscore

--- src/Application.js
+++ src/Application.js
@@ -1,51 +1,47 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import HighScore from './HighScore';
 
 class Application extends Component {
 
-    constructor(props){
+    constructor(props) {
         // Runs every time the component is instanciated
         super(props);
 
-        this.state ={
+        this.state = {
             count: 0,
             overTen: false
         }
-
-
     }
 
     handleClick = () => {
-        this.setState({count: this.state.count + 1});
+        this.setState({ count: this.state.count + 1 });
     }
 
-    componentDidUpdate(props, state){
+    componentDidUpdate(props, state) {
         // Gets call every time the component updates!!!!!!
         //console.log("Update from", state, "to", this.state);
-        if(this.state.count > 10 && this.state.count !== state.count && !this.state.overTen){
+        if (this.state.count > 10 && this.state.count !== state.count && !this.state.overTen) {
             console.log("Update overTen");
-            this.setState({overTen: true});
+            this.setState({ overTen: true });
         }
     }
 
-    render(){
+    render() {
 
         // Gets the count variable from the state
-        let {count} = this.state;
-        
-        return(
+        let { count } = this.state;
+
+        return (
             <div>
-            <h1>You clicked the button {count} times</h1>
-            {(this.state.overTen) ?
-                <h3>Beat high score of 10!</h3>
-                :null
-            }
-            
-            <span><button onClick={(e) => this.handleClick()}>
-                Click me
+                <h1>You clicked the button {count} times</h1>
+                <HighScore />
+
+                <span><button onClick={(e) => this.handleClick()}>
+                    Click me
                 </button></span>
             </div>
         )
-       
+
     }
 
 }
